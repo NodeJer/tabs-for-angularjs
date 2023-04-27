9d554ca5504b9dea3127716bcaf9e25abbad22ed
HuskyXH <wj@1245089789@163.com>
2023-04-27 16:19:04 +0800

    pr学习

--- angular-tabs.js
+++ angular-tabs.js
@@ -1,132 +1,141 @@
-angular.module('ng.tabs', []).
-
-directive('tabs', ['$compile', function($compile) {
-    return {
+angular
+  .module("ng.tabs", [])
+  //pr学习
+  .directive("tabs", [
+    "$compile",
+    function ($compile) {
+      return {
         scope: {},
-        controller: function($scope, $element) {
-            var panes = [];
-            var btns = [];
-            var templateElement;
+        controller: function ($scope, $element) {
+          var panes = [];
+          var btns = [];
+          var templateElement;
 
-            this.selectPane = function(pane) {
-                angular.forEach(panes, function(pane) {
-                    pane.selected = false;
-                });
-               pane.selected = true;
-            }
-            this.getScope = function(){
-            	return $scope;
-            }
-            this.addPane = function(pane) {
-                if (panes.length == 0) this.selectPane(pane);
-                panes.push(pane);
-            }
-            this.getPanes = function(){
-                return panes;
-            }
-            this.setBtnTemplateElement = function(el){
-                templateElement = el;
-            }
-            this.getBtnTemplateElement = function(){
-                return templateElement;
-            }
+          this.selectPane = function (pane) {
+            angular.forEach(panes, function (pane) {
+              pane.selected = false;
+            });
+            pane.selected = true;
+          };
+          this.getScope = function () {
+            return $scope;
+          };
+          this.addPane = function (pane) {
+            if (panes.length == 0) this.selectPane(pane);
+            panes.push(pane);
+          };
+          this.getPanes = function () {
+            return panes;
+          };
+          this.setBtnTemplateElement = function (el) {
+            templateElement = el;
+          };
+          this.getBtnTemplateElement = function () {
+            return templateElement;
+          };
         },
-        link: function($scope, $element, $attrs, controller){
-            
-            var templateElement = controller.getBtnTemplateElement();
-            var templateParent = templateElement.parent();  
-            var panes = controller.getPanes();
+        link: function ($scope, $element, $attrs, controller) {
+          var templateElement = controller.getBtnTemplateElement();
+          var templateParent = templateElement.parent();
+          var panes = controller.getPanes();
 
-            templateElement.removeAttr('tabs-btn-template');
+          templateElement.removeAttr("tabs-btn-template");
 
-            var outerHTML = templateElement[0].outerHTML;
+          var outerHTML = templateElement[0].outerHTML;
 
-            outerHTML = outerHTML.replace(/active\-class="(\w+)"/, 'ng-class="{$1: selected}"');
-            outerHTML = outerHTML.replace('ref', 'ng-bind');
+          outerHTML = outerHTML.replace(
+            /active\-class="(\w+)"/,
+            'ng-class="{$1: selected}"'
+          );
+          outerHTML = outerHTML.replace("ref", "ng-bind");
 
-            for(var i=0, len=panes.length; i<len; i++){
-                var pane = panes[i];
-                var newTemplateElement = angular.element(outerHTML);
+          for (var i = 0, len = panes.length; i < len; i++) {
+            var pane = panes[i];
+            var newTemplateElement = angular.element(outerHTML);
 
-                templateParent.append(newTemplateElement);
+            templateParent.append(newTemplateElement);
 
-                newTemplateElement.on('click', (function(pane){
-                    return function(){
-                        $scope.$apply(function(){
-                            controller.selectPane(pane);
-                        });
-                    }
-                })(pane));
+            newTemplateElement.on(
+              "click",
+              (function (pane) {
+                return function () {
+                  $scope.$apply(function () {
+                    controller.selectPane(pane);
+                  });
+                };
+              })(pane)
+            );
 
-                $compile(newTemplateElement)(pane);
-            }
-            //删除模板
-            templateElement.remove();
+            $compile(newTemplateElement)(pane);
+          }
+          //删除模板
+          templateElement.remove();
         },
-        restrict: 'AE',
-    };
-}]).
-
-directive('tabsBtnTemplate', ['$parse', function($parse){
-    return {
-        require: '^?tabs',
+        restrict: "AE",
+      };
+    },
+  ])
+  .directive("tabsBtnTemplate", [
+    "$parse",
+    function ($parse) {
+      return {
+        require: "^?tabs",
         scope: {},
-        restrict: 'AE',
-        link: function($scope, $element, iAttrs, tabsController) {
-            if(!tabsController)return;
+        restrict: "AE",
+        link: function ($scope, $element, iAttrs, tabsController) {
+          if (!tabsController) return;
 
-            tabsController.setBtnTemplateElement($element);
-        }
-    };
-}]).
-
-directive('tabsPane', ['$http', function($http) {
-    return {
-        require: '^?tabs',
-        restrict: 'AE',
+          tabsController.setBtnTemplateElement($element);
+        },
+      };
+    },
+  ])
+  .directive("tabsPane", [
+    "$http",
+    function ($http) {
+      return {
+        require: "^?tabs",
+        restrict: "AE",
         transclude: true,
         scope: {
-            title: '@',
-            ajax: '@'
+          title: "@",
+          ajax: "@",
         },
-        link: function($scope, $element, $attrs, tabsController) {
-            if(!tabsController)return;
+        link: function ($scope, $element, $attrs, tabsController) {
+          if (!tabsController) return;
 
-            tabsController.addPane($scope);
+          tabsController.addPane($scope);
 
-            $scope.$watch('selected', function(newValue, oldValue, scope) {
-            	if(newValue === true){
-                    $element.addClass($attrs.activeClass);
+          $scope.$watch("selected", function (newValue, oldValue, scope) {
+            if (newValue === true) {
+              $element.addClass($attrs.activeClass);
 
-                    if(!$scope.ajax)return;
+              if (!$scope.ajax) return;
 
-                    if($scope.ajaxData)return;
+              if ($scope.ajaxData) return;
 
-            		$http.post($scope.ajax).success(function(data){
-            			if(angular.isObject(data)){
-            				
-            			}
-            			else{
-            				$scope.ajaxData = data;
-            			}
-            		});
-            	}
-                else{
-                    $element.removeClass($attrs.activeClass);
+              $http.post($scope.ajax).success(function (data) {
+                if (angular.isObject(data)) {
+                } else {
+                  $scope.ajaxData = data;
                 }
-            });
-        },
-        template: function($element, $attrs){
-            var tagName = $element[0].tagName.toLowerCase();
-            var ajax = $attrs.ajax;
-
-            if(ajax && ajax != 'ajax'){
-                return '<'+tagName+' ng-bind="ajaxData"></'+tagName+'>';
+              });
+            } else {
+              $element.removeClass($attrs.activeClass);
             }
-
-            return '<'+tagName+' ng-transclude></'+tagName+'>';
-            
+          });
         },
-        replace: true
-    };
-}]);
+        template: function ($element, $attrs) {
+          var tagName = $element[0].tagName.toLowerCase();
+          var ajax = $attrs.ajax;
+
+          if (ajax && ajax != "ajax") {
+            return "<" + tagName + ' ng-bind="ajaxData"></' + tagName + ">";
+          }
+
+          return "<" + tagName + " ng-transclude></" + tagName + ">";
+        },
+        replace: true,
+      };
+    },
+  ]);
